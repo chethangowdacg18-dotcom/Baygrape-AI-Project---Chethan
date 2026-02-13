76b546da8cf8048e0e9e7a695fbc4f637df66c92
Chethan Gowda C R <chethangowda.cg18@gmail.com>
2026-02-13 17:04:56 +0530

    Update config.py

--- Baygrape-AI-Project---Chethan/git_ready_streamlit_clean/app/config.py
+++ Baygrape-AI-Project---Chethan/git_ready_streamlit_clean/app/config.py
@@ -1,12 +1,9 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
-GOOGLE_API_KEY = (
-    st.secrets.get("GEMINI_API_KEY")
-    if hasattr(st, "secrets") and "GEMINI_API_KEY" in st.secrets
-    else os.getenv("GOOGLE_API_KEY")
-)
+GOOGLE_API_KEY = st.secrets["GEMINI_API_KEY"]
 CHROMA_DB_DIR = os.getenv("CHROMA_DB_DIR", "./chroma_store")
 COLLECTION_NAME = "rag_documents"
